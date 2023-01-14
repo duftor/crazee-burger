fba6d73da4594424566373e97846ca9bb49fb285
Victor Dufayet <duftor@outlook.fr>
2023-01-14 23:01:23 +0100

    ✨ F01 : LOGIN > Add title and form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css';
 import LoginPage from './components/pages/LoginPage';
 
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function LoginPage() {
+  // State
+  const [name, setName] = useState("");
+
+  // Comportements
+  const handleChange = (e) => {
+      setName(e.target.value)
+  }
+
+  const handleSumbit = (e) => {
+      e.preventDefault();
+      alert("Bonjour " + name)
+      setName("")
+  }
+
+  // Affichage
   return (
-    <div>LoginPage</div>
-  )
+      <div>
+          <h1>Bienvenue chez nous !</h1>
+          <br />
+          <h2>Connectez-vous</h2>
+
+          <form onSubmit={handleSumbit}>
+              <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={name}
+                  placeholder="Entre votre prénom..."
+                  onChange={handleChange}
+                  required
+              />
+
+              <input type="submit" value="Accédez à votre espace" />
+          </form>
+      </div>
+  );
 }
