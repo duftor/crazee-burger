3c6e8cf6f198051742f8560f17241d8511782396
Victor Dufayet <duftor@outlook.fr>
2023-01-14 22:58:59 +0100

    ✨ F01 : Create LoginPage.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
 import { useState } from 'react';
 import './App.css';
+import LoginPage from './components/pages/LoginPage';
 
 function App() {
-    // State
-    const [name, setName] = useState("");
-
-    // Comportements
-    const handleChange = (e) => {
-        setName(e.target.value)
-    }
-
-    const handleSumbit = (e) => {
-        e.preventDefault();
-        alert("Bonjour " + name)
-        setName("")
-    }
-
-    // Affichage
     return (
-        <div>
-            <h1>Bienvenue chez nous !</h1>
-            <br />
-            <h2>Connectez-vous</h2>
-
-            <form onSubmit={handleSumbit}>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={name}
-                    placeholder="Entre votre prénom..."
-                    onChange={handleChange}
-                    required
-                />
-
-                <input type="submit" value="Accédez à votre espace" />
-            </form>
-        </div>
+        <LoginPage />
     );
 }
 
