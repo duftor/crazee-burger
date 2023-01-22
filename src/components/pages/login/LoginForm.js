7cc79874237df8884588036eddaf8d45fc0b04fd
Victor Dufayet <duftor@outlook.fr>
2023-01-23 00:22:29 +0100

    💄 F03 : LOGIN > LoginForm > add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,13 +1,12 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
-
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
+import { theme } from "../../theme/index"
 
 export default function LoginForm() {
     // State
     const [name, setName] = useState("")
     const navigate = useNavigate()
-
 
     // Comportements
     const handleChange = (e) => {
@@ -15,7 +14,7 @@ export default function LoginForm() {
     }
 
     const handleSumbit = (e) => {
-        e.preventDefault();
+        e.preventDefault()
         navigate("order/" + name)
     }
 
@@ -23,7 +22,7 @@ export default function LoginForm() {
     return (
         <LoginFormStyled onSubmit={handleSumbit}>
             <h1>Bienvenue chez nous !</h1>
-            <br />
+            <hr />
             <h2>Connectez-vous</h2>
             <input
                 type="text"
@@ -41,6 +40,28 @@ export default function LoginForm() {
 }
 
 const LoginFormStyled = styled.form`
-   background: green;
-   
-`;
+	background: green;
+	text-align: center;
+	max-width: 500px;
+	min-width: 400px;
+	margin: 0 auto;
+	padding: 2.5rem 2rem;
+	border-radius: ${theme.borderRadius.round};
+	font-family: "Amatic SC", cursive;
+
+	hr {
+		border: 1.5px solid #f56a2c;
+		margin-bottom: 40px;
+	}
+
+    h1 {
+        color: ${theme.colors.white};
+        font-size: ${theme.fonts.P5};
+    }
+
+    h2 {
+        color: ${theme.colors.white};
+        margin: 20px 10px 10px;
+        font-size: ${theme.fonts.P4};
+    }
+`
