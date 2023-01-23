18621bc637b105e2313c2df07ddf0393e0dfa8c5
Victor Dufayet <duftor@outlook.fr>
2023-01-23 01:13:30 +0100

    💄 F03 : LOGIN > LoginForm > button > add styling

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import { theme } from "../../theme/index"
 import { BsPersonCircle } from "react-icons/bs"
+import { IoChevronForward } from "react-icons/io5"
 
 export default function LoginForm() {
     // State
@@ -38,7 +39,10 @@ export default function LoginForm() {
                 />
             </div>
 
-            <input type="submit" value="Accéder à mon espace" />
+            <button className="button-with-icon">
+                <span>Accéder à mon espace</span>
+                <IoChevronForward className="icon" />
+            </button>
         </LoginFormStyled>
     )
 }
@@ -70,29 +74,74 @@ const LoginFormStyled = styled.form`
 	}
 
 	.input-with-icon {
-        background-color: white;
-        border-radius: 5px;
-        display: flex;
-        align-items: center;
-        padding: 18px 24px;
-        margin: 18px 0;
-        
-        .icon {
-            color: #93a2b1;
-            font-size: 15px;
-            margin-right: 8px;
-        }
+		background-color: white;
+		border-radius: 5px;
+		display: flex;
+		align-items: center;
+		padding: 18px 24px;
+		margin: 18px 0;
 
-        input {
-            border: none;
-            font-size: 15px;
-            color: #17161a;
-        }
+		.icon {
+			color: #93a2b1;
+			font-size: 15px;
+			margin-right: 8px;
+		}
 
-        &::placeholder {
-            background: white;
-            color: lightgrey;
-        }
+		input {
+			border: none;
+			font-size: 15px;
+			color: #17161a;
+            width: 100%;
+		}
+
+		&::placeholder {
+			background: white;
+			color: lightgrey;
+		}
 	}
 
+    .button-with-icon {
+        width: 100%;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
+        white-space: nowrap;
+        text-decoration: none;
+        line-height: 1;
+
+        padding: 18px 24px;
+        border-radius: 5px;
+        font-size: 15px;
+        font-weight: 800;
+        color: white;
+        background-color: #ff9f1b;
+        border: 1px solid #ff9f1b;
+
+        &:hover:not(:disabled) {
+            background-color: white;
+            color: #ff9f1b;
+            border: 1px solid #ff9f1b;
+            transition: all 200ms ease-out;
+        }
+
+        &:active {
+            color: white;
+            background-color: #ff9f1b;
+            border: 1px solid #ff9f1b;
+        }
+
+        &:disabled {
+            opacity: 0.6;
+            cursor: not-allowed;
+        }
+
+        .icon {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            font-size: 15px;
+            margin-left: 10px;
+        }
+    }
 `
