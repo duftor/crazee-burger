12e403c8b791b78b5bebc50e0560f30cc3d4c53f
Victor Dufayet <duftor@outlook.fr>
2023-01-25 18:50:33 +0100

    ♻️ F03 : LOGIN > LoginForm > Create PrimaryButton

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -5,6 +5,7 @@ import { theme } from "../../theme/index"
 import { IoChevronForward } from "react-icons/io5"
 import TextInput from "../../reusable-ui/TextInput"
 import { BsPersonCircle } from "react-icons/bs"
+import PrimaryButton from "./PrimaryButton"
 
 export default function LoginForm() {
 	// State
@@ -35,10 +36,11 @@ export default function LoginForm() {
 				Icon={<BsPersonCircle className="icon" />}
 				required
 			/>
-			<button className="button-with-icon">
-				<span>Accéder à mon espace</span>
-				<IoChevronForward className="icon" />
-			</button>
+
+			<PrimaryButton
+				label="Accéder à mon espace"
+				Icon={<IoChevronForward className="icon" />}
+			/>
 		</LoginFormStyled>
 	)
 }
@@ -68,48 +70,11 @@ const LoginFormStyled = styled.form`
 		font-size: ${theme.fonts.P4};
 	}
 
-	.button-with-icon {
-		width: 100%;
-		display: inline-flex;
+	.icon {
+		display: flex;
 		justify-content: center;
 		align-items: center;
-		position: relative;
-		white-space: nowrap;
-		text-decoration: none;
-		line-height: 1;
-
-		padding: 18px 24px;
-		border-radius: 5px;
 		font-size: 15px;
-		font-weight: 800;
-		color: white;
-		background-color: #ff9f1b;
-		border: 1px solid #ff9f1b;
-
-		&:hover:not(:disabled) {
-			background-color: white;
-			color: #ff9f1b;
-			border: 1px solid #ff9f1b;
-			transition: all 200ms ease-out;
-		}
-
-		&:active {
-			color: white;
-			background-color: #ff9f1b;
-			border: 1px solid #ff9f1b;
-		}
-
-		&:disabled {
-			opacity: 0.6;
-			cursor: not-allowed;
-		}
-
-		.icon {
-			display: flex;
-			justify-content: center;
-			align-items: center;
-			font-size: 15px;
-			margin-left: 10px;
-		}
+		margin-left: 10px;
 	}
 `
