8c3d2738118ba9a875af849f8c097c027d28f5b2
Victor Dufayet <duftor@outlook.fr>
2023-01-30 18:59:27 +0100

    ✨ F06 : ORDER > Navbar > NavbarRightSide > Implement Toast on button click

--- src/components/pages/order/Navbar/NavbarRightSide.js
+++ src/components/pages/order/Navbar/NavbarRightSide.js
@@ -1,11 +1,19 @@
 import styled from "styled-components"
 import Profile from "./Profile"
+import { ToastContainer, toast } from "react-toastify"
+
+import "react-toastify/dist/ReactToastify.css"
 
 export default function NavbarRightSide() {
+	const notify = () => toast("Wow so easy !")
+
 	return (
 		<NavbarRightSideStyled>
+			<button onClick={notify}>Notify !</button>
+
 			{/* <div className="admin-button">Admin Button</div> */}
 			<Profile />
+			<ToastContainer />
 		</NavbarRightSideStyled>
 	)
 }
