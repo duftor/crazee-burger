fec7edcd008ddc7e1b0d6a67b0a8194f431c6516
Victor Dufayet <duftor@outlook.fr>
2023-02-06 20:36:10 +0100

    ♻️ F07 : ORDER > AdminPanel > Hide/Show on admin mode

--- src/components/pages/order/Main/Tabs.js
+++ src/components/pages/order/Main/Tabs.js
@@ -7,13 +7,8 @@ import AdminPanelContext from "../../../../context/AdminPanelContext"
 import { theme } from "../../../../theme/index"
 
 export default function Tabs() {
-	const {
-		isAdminMode,
-		isPanelVisible,
-		setIsPanelVisible,
-		tabIndex,
-		setTabIndex,
-	} = useContext(AdminPanelContext)
+	const { isPanelVisible, setIsPanelVisible, tabIndex, setTabIndex } =
+		useContext(AdminPanelContext)
 
 	const toggleAdminPanel = () => {
 		setIsPanelVisible(!isPanelVisible)
@@ -29,49 +24,41 @@ export default function Tabs() {
 	}
 	return (
 		<TabsStyled>
-			{isAdminMode && (
-				<div className="tabs">
-					<div
-						className={`tab toggle ${!isPanelVisible && "selected"}`}
-						onClick={toggleAdminPanel}
-					>
-						{isPanelVisible ? (
-							<FiChevronDown className="icon" />
-						) : (
-							<FiChevronUp className="icon" />
-						)}
-					</div>
-					<div
-						className={`tab add-product ${tabIndex === 0 ? "selected" : ""}`}
-						onClick={onTab1Click}
-					>
-						<AiOutlinePlus className="icon" />
-						<span className="label">Ajouter un produit</span>
-					</div>
-					<div
-						className={`tab modify-product ${tabIndex === 1 ? "selected" : ""}`}
-						onClick={onTab2Click}
-					>
-						<MdModeEditOutline className="icon" />
-						<span className="label">Modifier un produit</span>
-					</div>
+			<div className="tabs">
+				<div
+					className={`tab toggle ${!isPanelVisible && "selected"}`}
+					onClick={toggleAdminPanel}
+				>
+					{isPanelVisible ? (
+						<FiChevronDown className="icon" />
+					) : (
+						<FiChevronUp className="icon" />
+					)}
 				</div>
-			)}
+				<div
+					className={`tab add-product ${tabIndex === 0 ? "selected" : ""}`}
+					onClick={onTab1Click}
+				>
+					<AiOutlinePlus className="icon" />
+					<span className="label">Ajouter un produit</span>
+				</div>
+				<div
+					className={`tab modify-product ${tabIndex === 1 ? "selected" : ""}`}
+					onClick={onTab2Click}
+				>
+					<MdModeEditOutline className="icon" />
+					<span className="label">Modifier un produit</span>
+				</div>
+			</div>
 		</TabsStyled>
 	)
 }
 
 const TabsStyled = styled.div`
-	/* position: absolute; */
-	/* bottom: 0;
-	/* width: 1400px; */
-
 	.icon {
 		width: 16px;
 		height: 16px;
 	}
-
-	/* border-bottom-left-radius: ${theme.borderRadius.extraRound}; */
 
 	.tabs {
 		margin-left: 71px;
@@ -85,51 +72,46 @@ const TabsStyled = styled.div`
 			justify-content: center;
 			background-color: ${theme.colors.white};
 
-			/* margin-right: 1px; */
+			margin-right: 1px;
 
-			/* border-width: 1px 1px 2px 1px; */
-			/* border-style: solid; */
-			/* border-color: ${theme.colors.greyLight}; */
-			/* box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.1); */
-			/* border-radius: 5px 5px 0px 0px; */
+			border-width: 1px 1px 2px 1px;
+			border-style: solid;
+			border-color: ${theme.colors.greyLight};
+			box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.1);
+			border-radius: 5px 5px 0px 0px;
 
-			/* color: ${theme.colors.greySemiDark}; */
-			/* font-weight: 400; */
-			/* font-size: 16px; */
+			color: ${theme.colors.greySemiDark};
+			font-weight: 400;
+			font-size: 16px;
 
-			/* cursor: pointer; */
+			cursor: pointer;
 		}
 		.tab:hover {
-			/* border-bottom-color: white; */
-			/* text-decoration: underline; */
+			border-bottom-color: white;
+			text-decoration: underline;
 		}
 
-		.tab.toggle {
-			/* padding: 13px 22px; */
-		}
-
-		.tab.add-product {
-			/* padding-right: 23px; */
+		.tab {
+			padding-right: 23px;
 			.icon {
-				/* padding: 13px 13px 13px 22px; */
+				padding: 13px 13px 13px 22px;
 			}
 		}
 
-		.tab.modify-product {
-			/* padding-right: 23px; */
-
+		.tab.toggle {
+			padding: 13px 22px;
 			.icon {
-				/* padding: 13px 13px 13px 22px; */
+				padding: 0;
 			}
 		}
 
 		.selected {
-			/* background-color: ${theme.colors.background_dark}; */
-			/* border-color: ${theme.colors.background_dark}; */
-			/* color: white; */
+			background-color: ${theme.colors.background_dark};
+			border-color: ${theme.colors.background_dark};
+			color: white;
 		}
 		.selected:hover {
-			/* border-bottom-color: ${theme.colors.background_dark}; */
+			border-bottom-color: ${theme.colors.background_dark};
 		}
 	}
 `
