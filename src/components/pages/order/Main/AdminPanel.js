891a6fcf6aa1fad839c477cd8e85138668e486af
Victor Dufayet <duftor@outlook.fr>
2023-02-06 20:14:42 +0100

    ♻️ F07 : ORDER > AdminPanel > Change panel style in order to don't hide scrollbar

--- src/components/pages/order/Main/AdminPanel.js
+++ src/components/pages/order/Main/AdminPanel.js
@@ -1,10 +1,43 @@
-import React from "react"
+import React, { useContext } from "react"
+import styled from "styled-components"
+import AdminPanelContext from "../../../../context/AdminPanelContext"
 import Tabs from "./Tabs"
+import { theme } from "../../../../theme/index"
 
 export default function AdminPanel() {
+	const { tabIndex } = useContext(AdminPanelContext)
+
 	return (
-		<div>
+		<AdminPanelStyled>
 			<Tabs />
-		</div>
+			<div className="content">
+				{tabIndex === 0 ? (
+					<div className="content-add-product">Ajouter un produit</div>
+				) : (
+					<div className="content-modify-product">Modifier un produit</div>
+				)}
+			</div>
+		</AdminPanelStyled>
 	)
 }
+
+const AdminPanelStyled = styled.div`
+	/* position: absolute;  // Cache la scrollbar
+	width: 1400px;
+	bottom: 2.5%; */
+
+	position: sticky;
+	bottom: 0;
+
+	.content {
+		height: 250px;
+		background-color: ${theme.colors.white};
+
+		border-bottom-left-radius: ${theme.borderRadius.extraRound};
+		/* border-bottom-right-radius: ${theme.borderRadius
+			.extraRound};   // Si position absolute */
+
+		border-top: 1px solid ${theme.colors.greyLight};
+		box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.2);
+	}
+`
--- src/components/pages/order/Main/Tabs.js
+++ src/components/pages/order/Main/Tabs.js
@@ -30,46 +30,31 @@ export default function Tabs() {
 	return (
 		<TabsStyled>
 			{isAdminMode && (
-				<div className="admin-panel">
-					<div className="tabs">
-						<div
-							className={`tab toggle ${!isPanelVisible && "selected"}`}
-							onClick={toggleAdminPanel}
-						>
-							{isPanelVisible ? (
-								<FiChevronDown className="icon" />
-							) : (
-								<FiChevronUp className="icon" />
-							)}
-						</div>
-						<div
-							className={`tab add-product ${tabIndex === 0 ? "selected" : ""}`}
-							onClick={onTab1Click}
-						>
-							<AiOutlinePlus className="icon" />
-							<span className="label">Ajouter un produit</span>
-						</div>
-						<div
-							className={`tab modify-product ${
-								tabIndex === 1 ? "selected" : ""
-							}`}
-							onClick={onTab2Click}
-						>
-							<MdModeEditOutline className="icon" />
-							<span className="label">Modifier un produit</span>
-						</div>
+				<div className="tabs">
+					<div
+						className={`tab toggle ${!isPanelVisible && "selected"}`}
+						onClick={toggleAdminPanel}
+					>
+						{isPanelVisible ? (
+							<FiChevronDown className="icon" />
+						) : (
+							<FiChevronUp className="icon" />
+						)}
 					</div>
-					{isPanelVisible && (
-						<div className="content">
-							{tabIndex === 0 ? (
-								<div className="content-add-product">Ajouter un produit</div>
-							) : (
-								<div className="content-modify-product">
-									Modifier un produit
-								</div>
-							)}
-						</div>
-					)}
+					<div
+						className={`tab add-product ${tabIndex === 0 ? "selected" : ""}`}
+						onClick={onTab1Click}
+					>
+						<AiOutlinePlus className="icon" />
+						<span className="label">Ajouter un produit</span>
+					</div>
+					<div
+						className={`tab modify-product ${tabIndex === 1 ? "selected" : ""}`}
+						onClick={onTab2Click}
+					>
+						<MdModeEditOutline className="icon" />
+						<span className="label">Modifier un produit</span>
+					</div>
 				</div>
 			)}
 		</TabsStyled>
@@ -77,16 +62,16 @@ export default function Tabs() {
 }
 
 const TabsStyled = styled.div`
-	position: absolute;
-	bottom: 0;
-	width: 1400px;
+	/* position: absolute; */
+	/* bottom: 0;
+	/* width: 1400px; */
 
 	.icon {
 		width: 16px;
 		height: 16px;
 	}
 
-	border-bottom-left-radius: ${theme.borderRadius.extraRound};
+	/* border-bottom-left-radius: ${theme.borderRadius.extraRound}; */
 
 	.tabs {
 		margin-left: 71px;
@@ -100,62 +85,51 @@ const TabsStyled = styled.div`
 			justify-content: center;
 			background-color: ${theme.colors.white};
 
-			margin-right: 1px;
+			/* margin-right: 1px; */
 
-			border-width: 1px 1px 2px 1px;
-			border-style: solid;
-			border-color: ${theme.colors.greyLight};
-			box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.1);
-			border-radius: 5px 5px 0px 0px;
+			/* border-width: 1px 1px 2px 1px; */
+			/* border-style: solid; */
+			/* border-color: ${theme.colors.greyLight}; */
+			/* box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.1); */
+			/* border-radius: 5px 5px 0px 0px; */
 
-			color: ${theme.colors.greySemiDark};
-			font-weight: 400;
-			font-size: 16px;
+			/* color: ${theme.colors.greySemiDark}; */
+			/* font-weight: 400; */
+			/* font-size: 16px; */
 
-			cursor: pointer;
+			/* cursor: pointer; */
 		}
 		.tab:hover {
-			border-bottom-color: white;
-			text-decoration: underline;
+			/* border-bottom-color: white; */
+			/* text-decoration: underline; */
 		}
 
 		.tab.toggle {
-			padding: 13px 22px;
+			/* padding: 13px 22px; */
 		}
 
 		.tab.add-product {
-			padding-right: 23px;
+			/* padding-right: 23px; */
 			.icon {
-				padding: 13px 13px 13px 22px;
+				/* padding: 13px 13px 13px 22px; */
 			}
 		}
 
 		.tab.modify-product {
-			padding-right: 23px;
+			/* padding-right: 23px; */
 
 			.icon {
-				padding: 13px 13px 13px 22px;
+				/* padding: 13px 13px 13px 22px; */
 			}
 		}
 
 		.selected {
-			background-color: ${theme.colors.background_dark};
-			border-color: ${theme.colors.background_dark};
-			color: white;
+			/* background-color: ${theme.colors.background_dark}; */
+			/* border-color: ${theme.colors.background_dark}; */
+			/* color: white; */
 		}
 		.selected:hover {
-			border-bottom-color: ${theme.colors.background_dark};
+			/* border-bottom-color: ${theme.colors.background_dark}; */
 		}
-	}
-
-	.content {
-		height: 250px;
-		background-color: ${theme.colors.white};
-
-		border-bottom-left-radius: ${theme.borderRadius.extraRound};
-		border-bottom-right-radius: ${theme.borderRadius.extraRound};
-
-		border-top: 1px solid ${theme.colors.greyLight};
-		box-shadow: 0px -2px 8px -2px rgba(0, 0, 0, 0.2);
 	}
 `
