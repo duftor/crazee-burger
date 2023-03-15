4059876dc933b0f404f1f171e1cd0d391828f38e
Victor Dufayet <duftor@outlook.fr>
2023-03-15 16:07:10 +0100

    💄 💄 💄 F08 : AddProductForm > Replace Flexbox by grid

--- src/components/pages/order/Main/Admin/AddProductForm.js
+++ src/components/pages/order/Main/Admin/AddProductForm.js
@@ -4,7 +4,6 @@ import { FaHamburger } from "react-icons/fa"
 import { BsFillCameraFill } from "react-icons/bs"
 import { MdOutlineEuro } from "react-icons/md"
 import { FiCheck } from "react-icons/fi"
-import { convertToNumber } from "../../../../../utils/maths"
 import { theme } from "../../../../../theme/index"
 import AdminContext from "../../../../../context/AdminContext"
 
@@ -45,85 +44,118 @@ export default function AddProductForm() {
 	}
 
 	return (
-		<AddProductFormStyled imageUrl={newProduct.imageSource}>
+		<AddProductFormStyled onSubmit={handleSubmit}>
 			<div className="image-div">
 				{newProduct.imageSource ? (
 					<img src={newProduct.imageSource} alt="" />
 				) : (
-					<div>Aucune image</div>
+					<div
+						className={`no-img-div ${!newProduct.imageSource && "has-border"}`}
+					>
+						Aucune image
+					</div>
 				)}
 			</div>
-			<form action="" onSubmit={handleSubmit}>
-				<div className="product-input">
-					<FaHamburger className="icon" color={theme.colors.greyBlue} />
-					<input
-						type="text"
-						name="title"
-						placeholder="Nom du produit (ex: Super Burger)"
-						value={newProduct.title}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="product-input">
-					<BsFillCameraFill className="icon" color={theme.colors.greyBlue} />
-					<input
-						type="url"
-						name="imageSource"
-						placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
-						value={newProduct.imageSource}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="product-input">
-					<MdOutlineEuro className="icon" color={theme.colors.greyBlue} />
-					<input
-						type="text"
-						name="price"
-						placeholder="Prix"
-						value={newProduct.price ? newProduct.price : ""}
-						onChange={handleChange}
-					/>
-				</div>
-				<div className="submit-btn">
-					<button>Ajouter un nouveau produit au menu</button>
-					{isSuccessDivVisible && (
-						<span className="success-div">
-							<FiCheck className="icon" />
-							Ajouté avec succès !
-						</span>
-					)}
-				</div>
-			</form>
+			<div className="product-input name">
+				<FaHamburger className="icon" color={theme.colors.greyBlue} />
+				<input
+					type="text"
+					name="title"
+					placeholder="Nom du produit (ex: Super Burger)"
+					value={newProduct.title}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="product-input url">
+				<BsFillCameraFill className="icon" color={theme.colors.greyBlue} />
+				<input
+					type="url"
+					name="imageSource"
+					placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+					value={newProduct.imageSource}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="product-input price">
+				<MdOutlineEuro className="icon" color={theme.colors.greyBlue} />
+				<input
+					type="text"
+					name="price"
+					placeholder="Prix"
+					value={newProduct.price ? newProduct.price : ""}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="submit-btn">
+				<button>Ajouter un nouveau produit au menu</button>
+				{isSuccessDivVisible && (
+					<span className="success-div">
+						<FiCheck className="icon" />
+						Ajouté avec succès !
+					</span>
+				)}
+			</div>
 		</AddProductFormStyled>
 	)
 }
 
-const AddProductFormStyled = styled.div`
+const AddProductFormStyled = styled.form`
 	padding-left: 71px;
 	padding-top: 31px;
 
 	width: 63%;
+	min-width: 650px;
+	/* height: 140px; */
 
-	display: flex;
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-template-rows: repeat(4, 35px);
+	grid-row-gap: 8px;
+
+	font-family: ${theme.fonts.family.secondary};
+	font-weight: ${theme.fonts.weights.regular};
+	font-size: ${theme.fonts.size.P0};
+	line-height: 17px;
+
+	.product-input.name {
+		grid-area: 1 / 2 / 2 / -1;
+	}
+	.product-input.url {
+		grid-area: 2 / 2 / 3 / -1;
+	}
+	.product-input.price {
+		grid-area: 3 / 2 / 4 / -1;
+	}
+	.submit-btn {
+		grid-area: 4 / 2 / 5 / -1;
+	}
 
 	.image-div {
+		grid-area: 1 / 1 / 4 / 2;
 		box-sizing: border-box;
-		border: ${(props) =>
-			props.imageUrl ? "none" : `1px solid ${theme.colors.greyLight}`};
 
-		width: 20%;
-		min-width: 110px;
+		width: 100%;
 		height: 120px;
 
 		display: flex;
 		align-items: center;
 		justify-content: center;
+		padding-right: 20px;
 
-		div {
+		.no-img-div {
 			font-weight: ${theme.fonts.weights.regular};
 			font-size: ${theme.fonts.size.P0};
 			line-height: 24px;
 			color: ${theme.colors.greySemiDark};
+		}
+		.has-border {
+			border: 1px solid ${theme.colors.greyLight};
+			width: 100%;
+			height: 100%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			border-radius: ${theme.borderRadius.round};
 		}
 
 		img {
@@ -133,101 +165,84 @@ const AddProductFormStyled = styled.div`
 		}
 	}
 
-	form {
-		height: 120px;
-		width: 100%;
-		padding-left: 20px;
-
+	.product-input {
 		display: flex;
-		flex-direction: column;
-		gap: 8px;
+		flex-direction: row;
+		align-items: center;
+
+		padding: 8px 16px 8px 24px;
+
+		background: ${theme.colors.background_white};
+		border-radius: ${theme.borderRadius.round};
+
+		.icon {
+			width: 15px;
+			height: 13px;
+			padding-right: 15px;
+		}
+
+		input {
+			padding: 1px 2px;
+			background-color: ${theme.colors.background_white};
+			border: none;
+			width: 100%;
+
+			color: ${theme.colors.dark};
+			font-size: ${theme.fonts.size.P0};
+		}
+
+		input::placeholder {
+			color: ${theme.colors.greyMedium};
+		}
+	}
+
+	button {
+		box-sizing: border-box;
+
+		background: ${theme.colors.success};
+		border: 1px solid ${theme.colors.success};
+		border-radius: ${theme.borderRadius.round};
+
+		width: 275px;
+
+		padding: 10px 29px;
 
 		font-family: ${theme.fonts.family.secondary};
-		font-weight: ${theme.fonts.weights.regular};
-		font-size: ${theme.fonts.size.P0};
-		line-height: 17px;
+		font-weight: ${theme.fonts.weights.bold};
+		font-size: ${theme.fonts.size.XS};
+		line-height: 14px;
+		align-items: center;
+		text-align: center;
 
-		.product-input {
+		color: ${theme.colors.white};
+	}
+	button:hover {
+		background: ${theme.colors.white};
+		border: 1px solid ${theme.colors.success};
+		color: ${theme.colors.success};
+
+		cursor: pointer;
+	}
+
+	.submit-btn {
+		display: flex;
+		.success-div {
+			padding-left: 5px;
+			margin-left: 10px;
+
+			color: ${theme.colors.success};
+			gap: 5px;
+
+			font-family: ${theme.fonts.family.tertiary};
+			font-weight: ${theme.fonts.weights.regular};
+			font-size: ${theme.fonts.size.P0};
+			line-height: 20px;
 			display: flex;
-			flex-direction: row;
 			align-items: center;
-			height: 35px;
-
-			padding: 8px 16px 8px 24px;
-			gap: 13px;
-
-			background: ${theme.colors.background_white};
-			border-radius: ${theme.borderRadius.round};
 
 			.icon {
-				width: 15px;
-				height: 13px;
-			}
-
-			input {
-				padding: 1px 2px;
-				background-color: ${theme.colors.background_white};
-				border: none;
-				width: 100%;
-
-				color: ${theme.colors.dark};
-				font-size: ${theme.fonts.size.P0};
-			}
-
-			input::placeholder {
-				color: ${theme.colors.greyMedium};
-			}
-		}
-
-		button {
-			box-sizing: border-box;
-
-			background: ${theme.colors.success};
-			border: 1px solid ${theme.colors.success};
-			border-radius: ${theme.borderRadius.round};
-
-			height: 35px;
-			width: fit-content;
-			padding: 10px 29px;
-
-			font-family: ${theme.fonts.family.secondary};
-			font-weight: ${theme.fonts.weights.bold};
-			font-size: ${theme.fonts.size.XS};
-			line-height: 14px;
-			align-items: center;
-			text-align: center;
-
-			color: ${theme.colors.white};
-		}
-
-		button:hover {
-			background: ${theme.colors.white};
-			border: 1px solid ${theme.colors.success};
-			color: ${theme.colors.success};
-
-			cursor: pointer;
-		}
-
-		.submit-btn {
-			display: flex;
-			.success-div {
-				padding-left: 5px;
-				margin-left: 10px;
-
-				color: ${theme.colors.success};
-				gap: 5px;
-
-				font-family: ${theme.fonts.family.tertiary};
-				font-weight: ${theme.fonts.weights.regular};
-				font-size: ${theme.fonts.size.P0};
-				line-height: 20px;
-				display: flex;
-				align-items: center;
-
-				.icon {
-					width: 18px;
-					height: 18px;
-				}
+				width: 18px;
+				height: 18px;
 			}
 		}
 	}
